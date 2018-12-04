c361474475ded283205c61549f75e9531a01d107
Teri <teri.eich@gmail.com>
2018-12-04 03:02:15 +0000

    switched printHTML function around

--- day1/scripting-http-req-node/module-test.js
+++ day1/scripting-http-req-node/module-test.js
@@ -1,9 +1,5 @@
 var getHTML = require('./make-it-a-module');
 
-function printHTML (html) {
-  console.log(html);
-}
-
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
