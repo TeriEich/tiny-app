858ed13ffadb9be9d7d42d7835f959a16934ad7b
Teri <teri.eich@gmail.com>
2018-12-05 00:21:06 +0000

    added urls_show.ejs and added route to express_server.js; working as intended; also adding some comments in the other files;

--- day2/tiny-app/express_server.js
+++ day2/tiny-app/express_server.js
@@ -25,7 +25,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// displays the list of URLs and their shortened forms
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// displays a single URL and its shortened form
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+                        urls: urlDatabase,
+                        shortURL: req.params.id
+                      };
+  res.render("urls_show", templateVars);
 });
